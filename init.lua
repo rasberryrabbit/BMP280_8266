cb68451b1d0ec829de45d18e6a1fce846cb3b7a5
rasberryrabbit <lovelust@hanmail.net>
2021-05-22 09:17:26 +0900

    Update.

--- init.lua
+++ init.lua
@@ -1,14 +1,14 @@
 if file.exists("dispsetup.lua") then 
     dofile("dispsetup.lua")
+    if file.exists("apconn.lua") then
+      dofile("apconn.lua")
+    end
     starttmr=tmr.create()
     tcount=6
     _G.gotip=false
     starttmr:register(1000, tmr.ALARM_AUTO,function()
       if tcount==0 then
         starttmr:unregister()
-        if file.exists("apconn.lua") then
-          dofile("apconn.lua")
-        end
         if file.exists("bmp280.lua") then
           dofile("bmp280.lua")
         end
